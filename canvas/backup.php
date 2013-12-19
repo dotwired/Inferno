469836d28441f4562b4e69e95a6ca60267856c3b
Maxim Zubarev <dotwired@gmail.com>
2013-12-20 00:37:43 +0100

    added backup default settings page

---
+++ canvas/backup.php
@@ -0,0 +1,15 @@
+<?php
+return array(
+    "title"  => "Backup",
+    "icon"   => "save",
+    "fields" => array(
+        array(
+            "id"    => "transfer",
+            "title" => "Import / Export your panel settings",
+            "desc"  => "Copy the content in the text area to a text file on your computer to save backup your current settings. To import settings, paste the previously copied content here again and press the 'import'-button.",
+            "more"  => "Note: You need to reload the current page to see actual changes in the fields. Also, no image files themselves are being recovered.",
+            "type"  => "transfer"
+        )
+    )
+)
+?>
